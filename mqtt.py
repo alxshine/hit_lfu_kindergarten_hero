5e4fd9f22af85b2cbd8486618f88087265929a21
Alexander Schlögl <alexander.schloegl@student.uibk.ac.at>
2019-06-05 23:27:23 +0200

    finished project

--- mqtt.py
+++ mqtt.py
@@ -1,4 +1,22 @@
 import paho.mqtt.client as mqtt
+import pygame
+
+pygame.mixer.init()
+bg = pygame.mixer.Sound('song/BG.wav')
+strings = pygame.mixer.Sound('song/STRINGS.wav')
+guitar = pygame.mixer.Sound('song/GUITAR.wav')
+sax = pygame.mixer.Sound('song/SAX.wav')
+
+bg.play(loops=-1)
+
+strings.set_volume(0)
+strings.play(loops=-1)
+
+guitar.set_volume(0)
+guitar.play(loops=-1)
+
+sax.set_volume(0)
+sax.play(loops=-1)
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -13,16 +31,28 @@ def on_message(client, userdata, msg):
     payload = bytes.decode(msg.payload)
     if payload == 'violin_down':
         print("unmute violin track")
+        strings.set_volume(1)
+        print("unmuted violin track")
     elif payload == 'violin_up':
         print("mute violin track")
+        strings.set_volume(0)
+        print("muted violin track")
     elif payload == 'guitar_down':
         print("unmute guitar track")
+        guitar.set_volume(1)
+        print("unmuted guitar track")
     elif payload == 'guitar_up':
         print("mute guitar track")
+        guitar.set_volume(0)
+        print("muted guitar track")
     elif payload == 'drum_down':
         print("unmute drum track")
+        sax.set_volume(1)
+        print("unmuted drum track")
     elif payload == 'drum_up':
         print("mute drum track")
+        sax.set_volume(0)
+        print("muted drum track")
     else:
         print("Unknown message: " + payload)
 
